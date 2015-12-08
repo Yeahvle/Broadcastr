66818e81edbbad82ad61995a6d996c7576e3e440
Ben Stein <ben.s.stein@gmail.com>
2015-12-08 12:34:50 -0600

    Initial commit.

---
+++ src/Broadcastr.Web/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Broadcastr.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // test comment for publish to git
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseIISPlatformHandler();
+
+            app.Run(async (context) => {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
